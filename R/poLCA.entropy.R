51ea0f91e52dbdb3de4026cf7c340afbb41d79f7
Claudio Bustos <clbustos@gmail.com>
2016-01-31 04:41:27 -0300

    Bug fix on poLCA.entropy.R: number of categories on each variables not always is the sample. Replace data.frame(sapply) for lapply

--- R/poLCA.entropy.R
+++ R/poLCA.entropy.R
@@ -1,7 +1,7 @@
 poLCA.entropy <-
 function(lc) {
     K.j <- sapply(lc$probs,ncol)
-    fullcell <- expand.grid(data.frame(sapply(K.j,seq,from=1)))
+    fullcell <- expand.grid(lapply(K.j,seq,from=1))
     P.c <- poLCA.predcell(lc,fullcell)
     return(-sum(P.c * log(P.c),na.rm=TRUE))
 }
